759ef763a0b99cd1a1cd7eca8abb5924771d3624
markusos <markus.ostberg@gmail.com>
2014-06-07 16:29:44 +0200

    minor optimisation and improved colouring

--- js/mandelbrot_worker.js
+++ js/mandelbrot_worker.js
@@ -4,14 +4,14 @@ var canvasData;
 
 onmessage = function(event) {
 	mandelbrot(event.data);
-} 
+}
 
 function mandelbrot(settings) {
 	var n;
 	var z;
 	var center = math.complex(settings.cx, settings.cy);
 	canvasData = settings.canvasData;
-		
+
 	for (var x = 0; x < settings.canvasWidth; x++){
 		for (var y = 0; y < settings.canvasHeight; y++) {
 			n = 0;
@@ -21,12 +21,17 @@ function mandelbrot(settings) {
 			c = math.add(center, math.complex(xi, yi));
 
 			while (math.abs(z) < 2 && n < settings.iter) {
-				z = math.add(math.multiply(z,z), c);
+				ztemp = math.add(math.multiply(z,z), c);
+        if (z === ztemp) {
+          n = settings.iter;
+          break;
+        }
+        z = ztemp;
 				n += 1;
 			}
 
 			var color = getColor(n, settings.iter);
-			updatePixel(x, y, color.r, color.g, color.b, 255);	
+			updatePixel(x, y, color.r, color.g, color.b, 255);
 		}
 	}
 	postMessage(canvasData);
@@ -41,31 +46,42 @@ function updatePixel (x, y, r, g, b, a) {
 }
 
 function getColor(n, maxn) {
-    var r = 0;
-    var g = 0;
-    var b = 0;
-    if (n != maxn) {
-    	var c = 3 * Math.log(n) / Math.log(maxn-1);
-    	if (c < 1)
-        {
-        	b = c;
-        }
-        else if (c < 2)
-        {
-        	g = c-1;
-        	b = 1;
-        }
-        else
-        {
-        	r = c-2;
-        	g = 1;
-         	b = 1;
-        }
-    }	
-    return {
-        r: math.floor(r * 255),
-        g: math.floor(g * 255),
-        b: math.floor(b * 255)
-    };
-}        
- 
+
+  var palette = [
+      {r:000,g:000,b:255},
+      {r:000,g:000,b:225},
+      {r:000,g:000,b:200},
+      {r:000,g:000,b:175},
+      {r:000,g:000,b:150},
+      {r:000,g:000,b:125},
+      {r:000,g:050,b:100},
+      {r:000,g:075,b:075},
+      {r:000,g:100,b:050},
+      {r:000,g:125,b:000},
+      {r:000,g:150,b:000},
+      {r:000,g:175,b:000},
+      {r:000,g:200,b:000},
+      {r:000,g:225,b:000},
+      {r:000,g:255,b:000},
+      {r:000,g:225,b:000},
+      {r:000,g:200,b:000},
+      {r:000,g:175,b:000},
+      {r:000,g:150,b:000},
+      {r:000,g:125,b:000},
+      {r:000,g:100,b:050},
+      {r:000,g:075,b:075},
+      {r:000,g:050,b:100},
+      {r:000,g:000,b:125},
+      {r:000,g:000,b:150},
+      {r:000,g:000,b:175},
+      {r:000,g:000,b:200},
+      ];
+
+  if (n === maxn) {
+    return {r:0,g:0,b:0}  
+  }    
+  else {
+    return palette[n%palette.length]
+  }
+}
+
