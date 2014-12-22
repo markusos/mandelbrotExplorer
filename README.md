4fcaa0c860725a255bae268ff40f85b541bcade8
markusos <markus.ostberg@gmail.com>
2014-12-22 15:03:34 -0500

    minor cleanup

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-mandelbrotExplorer
+Mandelbrot Explorer
 ======
 
 A JavaScript based Mandelbrot set explorer, using a Web Worker to calculate the Mandelbrot set.
 Click on the canvas to zoom in on that position or use the input fields to explore the fractal.
+
+![ScreenShot.png](img/ScreenShot.png)
--- js/mandelbrot_worker.js
+++ js/mandelbrot_worker.js
@@ -3,85 +3,84 @@ var math = mathjs();
 var canvasData;
 
 onmessage = function(event) {
-	mandelbrot(event.data);
-}
+    mandelbrot(event.data);
+};
 
 function mandelbrot(settings) {
-	var n;
-	var z;
-	var center = math.complex(settings.cx, settings.cy);
-	canvasData = settings.canvasData;
+    var n;
+    var z;
+    var center = math.complex(settings.cx, settings.cy);
+    canvasData = settings.canvasData;
 
-	for (var x = 0; x < settings.canvasWidth; x++){
-		for (var y = 0; y < settings.canvasHeight; y++) {
-			n = 0;
-			z = math.complex(0, 0);
-			xi = (x-settings.canvasWidth/2) / (settings.zoom * (settings.canvasWidth/2));
-			yi = (y-settings.canvasHeight/2) / (settings.zoom * (settings.canvasHeight/2));
-			c = math.add(center, math.complex(xi, yi));
+    for (var x = 0; x < settings.canvasWidth; x++){
+        for (var y = 0; y < settings.canvasHeight; y++) {
+            n = 0;
+            z = math.complex(0, 0);
+            xi = (x-settings.canvasWidth/2) / (settings.zoom * (settings.canvasWidth/2));
+            yi = (y-settings.canvasHeight/2) / (settings.zoom * (settings.canvasHeight/2));
+            c = math.add(center, math.complex(xi, yi));
 
-			while (math.abs(z) < 2 && n < settings.iter) {
-				ztemp = math.add(math.multiply(z,z), c);
+            while (math.abs(z) < 2 && n < settings.iter) {
+                ztemp = math.add(math.multiply(z,z), c);
         if (z === ztemp) {
           n = settings.iter;
           break;
         }
         z = ztemp;
-				n += 1;
-			}
+                n += 1;
+            }
 
-			var color = getColor(n, settings.iter);
-			updatePixel(x, y, color.r, color.g, color.b, 255);
-		}
-	}
-	postMessage(canvasData);
+            var color = getColor(n, settings.iter);
+            updatePixel(x, y, color);
+        }
+    }
+    postMessage(canvasData);
 }
 
-function updatePixel (x, y, r, g, b, a) {
-	var index = (x + y * canvasData.width) * 4;
-	canvasData.data[index + 0] = r;
-	canvasData.data[index + 1] = g;
-	canvasData.data[index + 2] = b;
-	canvasData.data[index + 3] = a;
+function updatePixel (x, y, color) {
+    var index = (x + y * canvasData.width) * 4;
+    canvasData.data[index + 0] = color.r;
+    canvasData.data[index + 1] = color.g;
+    canvasData.data[index + 2] = color.b;
+    canvasData.data[index + 3] = 255;
 }
 
 function getColor(n, maxn) {
 
   var palette = [
-      {r:000,g:000,b:255},
-      {r:000,g:000,b:225},
-      {r:000,g:000,b:200},
-      {r:000,g:000,b:175},
-      {r:000,g:000,b:150},
-      {r:000,g:000,b:125},
-      {r:000,g:050,b:100},
-      {r:000,g:075,b:075},
-      {r:000,g:100,b:050},
-      {r:000,g:125,b:000},
-      {r:000,g:150,b:000},
-      {r:000,g:175,b:000},
-      {r:000,g:200,b:000},
-      {r:000,g:225,b:000},
-      {r:000,g:255,b:000},
-      {r:000,g:225,b:000},
-      {r:000,g:200,b:000},
-      {r:000,g:175,b:000},
-      {r:000,g:150,b:000},
-      {r:000,g:125,b:000},
-      {r:000,g:100,b:050},
-      {r:000,g:075,b:075},
-      {r:000,g:050,b:100},
-      {r:000,g:000,b:125},
-      {r:000,g:000,b:150},
-      {r:000,g:000,b:175},
-      {r:000,g:000,b:200},
+      {r:0,g:0,b:255},
+      {r:0,g:0,b:225},
+      {r:0,g:0,b:200},
+      {r:0,g:0,b:175},
+      {r:0,g:0,b:150},
+      {r:0,g:0,b:125},
+      {r:0,g:50,b:100},
+      {r:0,g:75,b:75},
+      {r:0,g:100,b:50},
+      {r:0,g:125,b:0},
+      {r:0,g:150,b:0},
+      {r:0,g:175,b:0},
+      {r:0,g:200,b:0},
+      {r:0,g:225,b:0},
+      {r:0,g:255,b:0},
+      {r:0,g:225,b:0},
+      {r:0,g:200,b:0},
+      {r:0,g:175,b:0},
+      {r:0,g:150,b:0},
+      {r:0,g:125,b:0},
+      {r:0,g:100,b:50},
+      {r:0,g:75,b:75},
+      {r:0,g:50,b:100},
+      {r:0,g:0,b:125},
+      {r:0,g:0,b:150},
+      {r:0,g:0,b:175},
+      {r:0,g:0,b:200},
       ];
 
   if (n === maxn) {
-    return {r:0,g:0,b:0}  
-  }    
+    return {r:0,g:0,b:0}
+  }
   else {
     return palette[n%palette.length]
   }
 }
-
